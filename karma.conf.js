c3f0517beeb8a49ff822fc0b3dc155e47e6eede3
¨Costel <costel.radulescu.cs@gmail.com>
2018-05-07 20:38:39 +0200

    fixed ci

--- karma.conf.js
+++ karma.conf.js
@@ -34,6 +34,6 @@ module.exports = function (config) {
         flags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
       }
     },
-    singleRun: false
+    singleRun: true
   });
 };
