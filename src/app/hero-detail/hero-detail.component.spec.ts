6c287b7b5da9a478c741bd6d5e14040086e6c5e7
¨Costel <costel.radulescu.cs@gmail.com>
2018-05-08 12:40:37 +0200

    fixed failing test

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Location } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Observable, ReplaySubject } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { of } from 'rxjs/observable/of';
 import {
